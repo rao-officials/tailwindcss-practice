03359d157965ffb997e6ce799f4c3df176ebc10a
rao-officials <rao.officials@gmail.com>
2022-07-05 21:01:35 +0500

    Create multiple sections for practice purpose eg , hero section , sidebar ,text clipping etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwindcss-practice';
+  flag:boolean = false
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'image1': "url('./src/assets/images/1.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
   },
   plugins: [],
 }
+
+
