c0d39dd649122d3dff6783883595b12e64332ae1
rao-officials <rao.officials@gmail.com>
2022-07-06 20:31:08 +0500

    start working at portfolio ,  Add portfolio navbar & header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       backgroundImage: {
-        'image1': "url('./src/assets/images/1.jpg')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      }
-    }
+        image1: "url('./src/assets/images/1.jpg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(4deg)",
+          },
+          "75%": {
+            transform: "rotate(-4deg)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
-
+};
