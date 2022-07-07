2e79528bb4b76cc722f2f55d597674d7ec176bd9
rao-officials <rao.officials@gmail.com>
2022-07-07 17:53:47 +0500

    create projects section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SliderComponent,
+    ServicesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,40 @@ module.exports = {
       },
       animation: {
         tilt: "tilt 10s infinite linear",
+        tiltTwo: "tiltTwo 100s infinite linear",
       },
       keyframes: {
         tilt: {
           "0%, 50%, 100%": {
-            transform: "rotate(0deg)",
+            transform: "rotate(-5deg)",
           },
           "25%": {
             transform: "rotate(4deg)",
           },
           "75%": {
-            transform: "rotate(-4deg)",
+            transform: "rotate(5deg)",
+          },
+        },
+        tiltTwo: {
+          
+          "0%" :{
+            transform: "rotate(0deg)",
+          },
+
+          "25%": {
+            transform: "rotate(95deg)",
+          },
+
+          "50%": {
+            transform: "rotate(195deg)",
+          },
+
+          "75%": {
+            transform: "rotate(285deg)",
+          },
+
+          "100%": {
+            transform: "rotate(360deg)",
           },
         },
       },
